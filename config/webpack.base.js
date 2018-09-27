c2b6ca1d772c4c03fa0594b46ba278b79f98fe17
shichencong <chencong.shi@geely.com>
2018-09-27 16:08:11 +0800

    add less handle

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const dir = path.join(__dirname, '..'); //拿到根目录 /Users/scc/draft4/react-boilerplate
 
 module.exports = {
   mode: 'development',
   entry: {
-    app: ['webpack-hot-middleware/client?noInfo=true&reload=true' , './src'],
+    app: ['webpack-hot-middleware/client?noInfo=true&reload=true', './src'],
   },
   output: {
     path: path.join(dir, 'build'),
@@ -25,6 +26,19 @@ module.exports = {
             // plugins: ['react-hot-loader/babel'],
           }
         }
+      },
+      {
+        test: /\.(css|less)$/,
+        use: [
+          'css-hot-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: '../'
+            }
+          }, 'css-loader', 'less-loader']
       }
     ]
   },
@@ -33,5 +47,11 @@ module.exports = {
       inject: true,
       template: './index.html'
     }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
   ]
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ReactDom from 'react-dom';
+import './index.less';
 
 export default class App extends Component {
   render() {
